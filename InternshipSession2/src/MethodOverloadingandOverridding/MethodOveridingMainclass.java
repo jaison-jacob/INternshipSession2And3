4e0cb7cf8371f0497f13ef66445287024fa42a5f
Jaison Jacob <jaisonjai250795@gmail.com>
2020-11-09 21:29:26 +0530

    "it is array,string method,method overloading,overiding"

---
+++ InternshipSession2/src/MethodOverloadingandOverridding/MethodOveridingMainclass.java
@@ -0,0 +1,20 @@
+package MethodOverloadingandOverridding;
+
+public class MethodOveridingMainclass {
+
+	public static void main(String arg[]){
+		
+		Bclass ex =new Bclass();
+		
+		System.out.println("method overridding : ");
+		 ex.output();
+		 //output A
+		 ex.outputA();
+		 
+		 //output B
+		 
+		 ex.outputB();
+		
+	}
+	
+}
